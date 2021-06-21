94d6dc68b268439a51b06a3f83e205d7dd25b68a
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2021-06-21 20:44:06 -0300

    criaçao pastas e model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarComponent } from './components/views/listar/listar.component';
+import { CadastrarComponent } from './components/views/cadastrar/cadastrar.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { ContentComponent } from './components/template/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarComponent
+    ListarComponent,
+    CadastrarComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/livro.ts
@@ -0,0 +1,8 @@
+export class Livro{
+    _id?: String;
+    nome!: String;
+    isbn!: String
+    preco!: Number;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
